5834c669521f728d6475c9415b52bf17d24aa375
GIALAM122 <son9ahoang@gmail.com>
2024-08-06 13:27:37 +0700

    hehe

--- reactproject/vite.config.js
+++ reactproject/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactproject/",
+  base: "/",
   build: {
     outDir: "./docs",
     emptyOutDir: true, // also necessary
